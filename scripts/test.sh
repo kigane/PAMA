ee59c6b37a19bc0d1cf4e14ff4eb9ec93ae3cd29
kigane <huwkigane@outlook.com>
2022-06-07 14:16:48 +0800

    fix test scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
 set -ex
-python main.py eval --content ./data/val/1.jpg --style ./data/Shinkai/style/0_1.jpg
+python main.py --pretrained eval --content ./data/val/1.jpg --style ./data/Shinkai/style/0_1.jpg
